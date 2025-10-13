e1f564ea6ba6aa7d4aae2d637d4ae3a209cca19f
Kevin <kolioane@gmail.com>
2025-10-13 12:26:52 -0700

    Update Program.cs w/ services + repo + handlers +  update appsettings.Development.json + appsettings.json

--- src/wizard-of-auth.API/Program.cs
+++ src/wizard-of-auth.API/Program.cs
@@ -1,9 +1,64 @@
+using FluentValidation;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.EntityFrameworkCore;
+using wizard_of_auth.Core.Interfaces;
+using wizard_of_auth.Infrastructure.Persistence;
+using wizard_of_auth.Infrastructure.Protocols.OAuth2;
+using wizard_of_auth.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// MediatR
+builder.Services.AddMediatR(cfg 
+    => cfg.RegisterServicesFromAssembly(typeof(ApplicationDbContext).Assembly));
+
+// FluentValidation
+builder.Services.AddValidatorsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+// Services
+// builder.Services.AddScoped<ITokenService, TokenService>();
+// builder.Services.AddScoped<IUserService, UserService>();
+// builder.Services.AddScoped<IClientService, ClientService>();
+// builder.Services.AddScoped<IAuditLogService, AuditLogService>();
+// builder.Services.AddScoped<IIdentityProviderService, IdentityProviderService>();
+// builder.Services.AddScoped<ITenantService, TenantService>();
+// builder.Services.AddScoped<IEmailService, EmailService>();
+
+// Repositories
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+
+// Protocol Handlers
+builder.Services.AddScoped<OAuthHandler>();
+// builder.Services.AddScoped<OidcConnectHandler>();
+// builder.Services.AddScoped<SamlHandler>();
+
+// Security
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddFixedWindowLimiter("login", opt =>
+    {
+        opt.PermitLimit = 5;
+        opt.Window = TimeSpan.FromMinutes(1);
+    });
+});
+
+// Cahcing
+// builder.Services.AddMemoryCache();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+    options.InstanceName = "wizard_of_auth";
+});
 
 var app = builder.Build();
 
@@ -15,30 +70,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+// app.UseSecurityHeaders(); // Custom middleware
+app.UseRateLimiter();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
